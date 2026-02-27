5749e967338931a1bd115831e051b373fad3d815
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-02-27 15:55:52 -0300

    cor botao visivel

--- migrate/dados_de_exemplo.sql
+++ migrate/dados_de_exemplo.sql
@@ -18,11 +18,7 @@ INSERT INTO `youã`.`musica`
 `nome_genero`,
 `stats`)
 VALUES
-("System of a Down","00:02:34","Sugar","https://i.scdn.co/image/ab67616d0000b2734e7ff4a30597e57f5bc988f9","Rock", "ATIVO"),
-("System of a Down","00:04:09","Radio/Video","https://cdn-images.dzcdn.net/images/cover/4954f43cc6033ddfa0fa3ee5514a26ca/500x500.jpg","Rock","ATIVO"),
-("System of a Down","00:02:47","Lonely Day","https://upload.wikimedia.org/wikipedia/pt/5/50/220px-System_Of_A_Down-Hypnotize.jpg","Rock","ATIVO"),
-("System of a Down","00:03:45","Psycho","https://i.scdn.co/image/ab67616d00001e02d702e708c6b48247ba167d04","Rock","ATIVO"),
-("Chico Buarque","00:04:01","Cálice","https://upload.wikimedia.org/wikipedia/pt/5/59/Chico_buarque_1978.jpg","MPB","ATIVO"),
-("Cássia Eller","00:04:11","O Segundo Sol","https://cdn-images.dzcdn.net/images/cover/9222d5c4c7e488199b0134531dc8328c/1900x1900-000000-80-0-0.jpg","Pop","ATIVO"),
-("Arctic Monkeys","00:04:13","505","https://cdn-images.dzcdn.net/images/cover/d7a4f9f1af8736457de34f28d50ef496/1900x1900-000000-80-0-0.jpg","Indie","ATIVO");
+("Justin Bieber","00:02:34","Eenie Meenie","https://cdn-images.dzcdn.net/images/cover/b6462d90a95934d447a8a1c1a6d5f245/0x1900-000000-80-0-0.jpg","Pop", "ATIVO"),
+("Tame Impala ","00:03:38","The Less I Know The Better","https://m.media-amazon.com/images/I/91dTLHSQdkL.jpg","Rock","ATIVO"),
+("Fresno","00:03:56","Quebre as correntes","https://images.genius.com/5615192b37a6ff6553743cd73569eef1.999x999x1.png","Rock","ATIVO");
 
