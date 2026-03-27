3abb7dc486c43630e77b39e5441bfc7c68eea388
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-03-27 15:52:07 -0300

    Refactor database connection to handle local and cloud
    Updated the connection logic to support both local and cloud databases based on connection type.

--- database/conexao.py
+++ database/conexao.py
@@ -1,15 +1,23 @@
 import mysql.connector
 
 def conectar():
-    
-    conexao=mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="root",
-            database="youã"
-        )
-    
+    tipo_conexao = "NUVEM"
+    if tipo_conexao == "LOCAL":
+        conexao=mysql.connector.connect(
+                host="localhost",
+                port=3306,
+                user="root",
+                password="root",
+                database="youã"
+            )
+    else:
+        conexao=mysql.connector.connect(
+                host="servidor-pablo-servidor-pablo.a.aivencloud.com",
+                port=12176,
+                user="avnadmin",
+                password="AVNS_VFMgYg6Tyz27bFOtd4N",
+                database="youã"
+            )
     cursor=conexao.cursor(dictionary=True)
 
     return conexao, cursor
